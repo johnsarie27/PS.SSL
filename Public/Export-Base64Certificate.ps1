3b148b2ece6cc3068e9bd18d67318c37181c9b8c
jjohns <jjohns@esri.com>
2024-06-27 08:55:28 -0700

    updated comments and parameter validation

--- Public/Export-Base64Certificate.ps1
+++ Public/Export-Base64Certificate.ps1
@@ -1,29 +1,25 @@
 function Export-Base64Certificate {
     <#
     .SYNOPSIS
-        Convert a byte array to a base64 encoded string
+        Convert a byte array to a base64 encoded certificate file
     .DESCRIPTION
-        Convert a byte array to a base64 encoded string
+        Convert a byte array to a base64 encoded certificate file
     .PARAMETER ByteArray
         Byte array
+    .PARAMETER Path
+        Path to output certificate file
     .INPUTS
         None.
     .OUTPUTS
-        System.String.
+        None.
     .EXAMPLE
         PS C:\> $cert = Get-RemoteSSLCertificate -ComputerName 'example.com'
-        PS C:\> Export-Base64Certificate -ByteArray $cert.RawData
-        Convert the remote SSL certificate byte array to a base64 encoded string
-    .EXAMPLE
-        PS C:\> $b64s = Export-Base64Certificate -ByteArray (Get-RemoteSSLCertificate -ComputerName 'example.com').RawData
-        PS C:\> $pubCert = @('-----BEGIN CERTIFICATE-----')
-        PS C:\> $pubCert += for ($i = 0; $i -LT $b64s.Length; $i += 64) { $b64s.Substring($i, 64) }
-        PS C:\> $pubCert += '-----END CERTIFICATE-----'
-        Gets the SSL certificate from example.com and converts the raw data to the proper Base64 encoded certificate format
+        PS C:\> Export-Base64Certificate -ByteArray $cert.RawData -Path "$HOME\Desktop\example.com.crt"
+        Convert the remote SSL certificate byte array to a base64 encoded certificate file and save to the desktop
     .NOTES
         Name:     Export-Base64Certificate
         Author:   Justin Johns
-        Version:  0.1.0 | Last Edit: 2024-04-19
+        Version:  0.1.1 | Last Edit: 2024-06-27
         - Version history is captured in repository commit history
         Comments: <Comment(s)>
     #>
@@ -34,9 +30,10 @@ function Export-Base64Certificate {
         [ValidateNotNullOrEmpty()]
         [System.Byte[]] $ByteArray,
 
-        [Parameter(Mandatory, Position = 1, HelpMessage = 'Path to output certificate file')]
+        [Parameter(Mandatory = $true, Position = 1, HelpMessage = 'Path to output certificate file')]
         [ValidateScript({ Test-Path -Path (Split-Path -Path $_) -PathType Directory })]
         [ValidatePattern('^.+\.crt$')]
+        [ValidateScript({ -Not (Test-Path -Path $_) })] # ENSURE FILE DOES NOT EXIST
         [System.String] $Path
     )
     Begin {
